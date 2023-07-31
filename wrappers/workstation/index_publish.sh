ec02746ff4d498a6d816f188982f0e5f9893b7cb
lukaszlacinski <lukasz@uchicago.edu>
2023-07-31 13:35:36 -0500

    Simplify index_publish wrappers

--- wrappers/workstation/index_publish.sh
+++ wrappers/workstation/index_publish.sh
@@ -2,4 +2,10 @@
 
 /home/lukasz/miniconda3/envs/esgf-pub/bin/esgindexpub --pub-rec $1 --no-auth >> /tmp/esgf2_log.txt
 
-/opt/esgf2/wrappers/index_publish.py $2
+# ESGF2 prod index
+# globus_index_id="d927e2d9-ccdb-48e4-b05d-adbc3d97bbc5"
+
+# ESGF2 test index
+globus_index_id="4936364c-f366-4436-8fc5-99b4beafb3c9"
+
+/opt/esgf2/venv/bin/globus search ingest $globus_index_id $2
